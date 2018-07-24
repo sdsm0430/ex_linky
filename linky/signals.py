1a7bbef978fe365b8817d6996d81a0f7a28a6a89
linkey <skdy3333@gmail.com>
2018-07-24 14:25:54 +0900

    now add script done

--- linky/signals.py
+++ linky/signals.py
@@ -3,28 +3,25 @@ from django.dispatch import receiver
 from django.conf import settings
 from linky.models import Musical, Script
 import csv, os
+from django.db import transaction
 
+@transaction.atomic
 @receiver(post_save, sender=Musical)
 def renew_scripts(sender, **kwargs):
     musical = kwargs['instance']
     if(musical.csv):
-        # 먼저 script 잡고 다 지움
         scrts = Script.objects.filter(musical=musical)
         for scr in scrts:
             scr.delete()
-        # 이제 저장
         with open(os.path.join(settings.BASE_DIR, os.path.join(settings.MEDIA_ROOT, musical.csv.name)), 'r') as f:
             rd = csv.reader(f)
             for line in rd:
-                try:
-                    idx, lang, song, actor, text = line
-                    if(idx == "순서"):  # 가장 위의 index일 때
-                        continue
-                    else:
-                        Script.objects.create(musical=musical, language=lang,
-                                              order=idx, music=song, actor=actor, song=text)
-                except:  # 잘못된 line
-                    print("잘못되었습니다")
+                idx, lang, song, actor, text = line
+                if(idx == "idx"):  # 가장 위의 index일 때
+                    continue
+                else:
+                    Script.objects.create(musical=musical, language=lang,
+                     order=idx, music=song, actor=actor, song=text)
 
     else:
         # csv가 없을 때 do nothing
